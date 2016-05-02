194f4e86994a115cc0c253a868bb035d4216cb59
Daiwei <daiweilu123@gmail.com>
2016-05-02 16:06:49 -0400

    Improve deployment step

---
+++ scripts/deploy.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const join = require('path').join;
+const basename = require('path').basename;
+const Promise = require('bluebird');
+const glob = Promise.promisify(require('glob'));
+const co = require('co');
+const R = require('ramda');
+const gcloud = require('gcloud')({
+  projectId: 'starboard-1277',
+  keyFilename: join(__dirname, '../local/starboard-ui-deploy.json'),
+});
+
+const gcs = gcloud.storage();
+const bucket = gcs.bucket('static.getstarboard.xyz');
+
+Promise.promisifyAll(Object.getPrototypeOf(bucket));
+
+const DEFAULT = {
+  metadata: {
+    acl: [
+      {
+        entity: 'allUsers',
+        role: gcs.acl.READER_ROLE
+      }
+    ],
+    cacheControl: 'no-cache'
+  }
+};
+
+const mergeDest = R.merge(DEFAULT);
+
+co(function *() {
+  const files = yield glob(join(__dirname, '../public/*.js*'));
+  yield Promise.all(files.map(upload));
+  yield upload(join(__dirname, '../public/index.css'));
+  yield upload(join(__dirname, '../webpack-assets.json'));
+})
+.catch(console.error);
+
+function upload(file) {
+  console.log(`Uploading ${file}`);
+  return bucket.uploadAsync(file, mergeDest({desination: basename(file)}));
+}
