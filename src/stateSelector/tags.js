41fdcfb33855bee72cfab45f6b4b3741342fe77e
Daiwei <daiweilu123@gmail.com>
2016-06-06 15:28:37 -0400

    Fix not removing selected tags from tag list

--- src/stateSelector/tags.js
+++ src/stateSelector/tags.js
@@ -1,16 +1,23 @@
+import {prop, pipe, pluck, values, filter, contains, __, not, sortBy, reverse,
+  propEq, map} from 'ramda';
 import {createSelector}          from 'reselect';
-import {prop, pipe, map, values, filter, contains, __, not, sortBy, reverse} from 'ramda';
+import {TAG_FILTER}              from '../const/FILTER_TYPES';
 import {assignDefaultColorToTag} from './Utils';
 
 export default createSelector(
   prop('filters'),
   prop('tagsById'),
   (filters, tagsById) => {
+    const selectedTagIds = pipe(
+      filter(propEq('type', TAG_FILTER)),
+      pluck('tagId')
+    )(filters);
+
     return pipe(
       values,
       filter(pipe(
         prop('id'),
-        contains(__, filters),
+        contains(__, selectedTagIds),
         not
       )),
       sortBy(prop('id')),
