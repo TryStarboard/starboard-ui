8e993c4f6653ee521cb2cb0543b618913082499f
Daiwei <daiweilu123@gmail.com>
2016-06-07 08:48:12 -0400

    Teach searching to ignore cases

--- src/stateSelector/repos.js
+++ src/stateSelector/repos.js
@@ -1,19 +1,32 @@
 import {createSelector} from 'reselect';
-import {prop, pipe, map, values, filter, contains, sortBy, reverse, allPass, any} from 'ramda';
+import {prop, pipe, map, values, filter, contains, sortBy, reverse, allPass,
+  any, test} from 'ramda';
 import {TAG_FILTER} from '../const/FILTER_TYPES';
+
+function escapeStringForRegex(s) {
+  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
 
 export default createSelector(
   prop('filters'),
   prop('reposById'),
-  (filters, reposById) => {
+  prop('tagsById'),
+  (filters, reposById, tagsById) => {
     const isPassAllFilter = allPass(filters.map((filterObj) => {
       if (filterObj.type === TAG_FILTER) {
         return pipe(prop('tags'), contains(filterObj.tagId));
+
       } else {
         return ({full_name, description, tags}) => {
-          return full_name.indexOf(filterObj.text) > -1 ||
-            (description && description.indexOf(filterObj.text) > -1) ||
-            any(contains(filterObj.text), tags);
+          const regex = new RegExp(escapeStringForRegex(filterObj.text), 'i');
+
+          return regex.test(full_name) ||
+            (description && regex.test(description)) ||
+            any(pipe(
+              (tagId) => tagsById[tagId],
+              prop('text'),
+              test(regex)
+            ), tags);
         };
       }
     }));
